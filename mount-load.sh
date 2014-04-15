40f9846350ac4b9eb686cc90b041059467ba16d2
Mark G. Whitney <markwhitney@gmail.com>
2014-04-14 23:15:26 -0400

    some sanity checks on input

--- mount-load.sh
+++ mount-load.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 target=$1
+if [ -z "$target" ]; then
+	target=${HOME}
+fi
+
+if [ ! -e ${target}/secmnt ]; then
+	echo "missing ${target}/secmnt, exiting"
+	exit 1
+fi
 
 # only mount if mountpoint looks unused
-find ~/.secmnt -maxdepth 0 -empty -exec encfs ${target}/secmnt ~/.secmnt \;
+find ~/.secmnt -maxdepth 0 -empty -exec encfs --idle=20 ${target}/secmnt ~/.secmnt \;
 
 source ~/.aws/profile
 
